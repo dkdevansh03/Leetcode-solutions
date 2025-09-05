60c77021245c46934d86d2958592827012348c21
Devansh Gupta <0221csai232@niet.co.in>
2025-09-05 16:30:06 +0530

    Time: 6 ms (8.01%) | Memory: 23.5 MB (5.33%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int>s;
+        for(int i = 0 ; i<nums.size() ; i++){
+            s.insert(nums[i]);
+        }
+        int k = s.size();
+        int j = 0;
+        for(auto it : s){
+            nums[j]=it;
+            j++;
+        }
+        return k;
+    }
+};
