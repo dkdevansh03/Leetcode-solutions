2f62b2b5ddae6686d28569d81a2ded2f08954055
Devansh Gupta <0221csai232@niet.co.in>
2025-09-12 18:48:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (64.46%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,14 +1,18 @@
 class Solution {
+private:
+    vector<int> generateRows(int n) {
+        vector<int> res(n);
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = (long long)res[i - 1] * (n - i) / i;
+        }
+        return res;
+    }
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> output;
-        for(int  i = 0 ; i < numRows ; i++){
-            vector<int>ans(i+1);
-            ans[0]=1;
-            for(int j = 1 ; j < i+1 ; j++){
-            ans[j] = (long long)ans[j-1]*(i-j+1)/j;
-            }
-            output.push_back(ans);    
+        for (int i = 1; i <= numRows; i++) {
+            output.push_back(generateRows(i));
         }
         return output;
     }
