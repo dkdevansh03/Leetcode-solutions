b88595b3dacc2a401568bf8d689c7bf512bd0f5f
Devansh Gupta <0221csai232@niet.co.in>
2025-09-25 00:12:39 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (47.20%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int, int> mpp;
-        int mini = nums.size() / 3 + 1;
-        for (int i = 0; i < nums.size(); i++) {
-            mpp[nums[i]]++;
-
-            if (mpp[nums[i]] == mini) {
-                ans.push_back(nums[i]);
+        //BOYER MOORE'S ALGORITHM
+        int n  = nums.size();
+        int cnt1 = 0 , cnt2 = 0;
+        int el1 = INT_MIN , el2 = INT_MIN;
+        for(int i = 0 ; i<n ; i++){
+            if(cnt1==0 && nums[i]!=el2){
+                el1 = nums[i];
+                cnt1 = 1;
             }
-
-            if (ans.size() == 2)
-                break;
+            else if (cnt2 == 0 && nums[i]!=el1){
+                el2 = nums[i];
+                cnt2 = 1;
+            }
+            else if (nums[i]==el1) cnt1++;
+            else if (nums[i]==el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
-        return ans;
+        vector<int>res;
+        cnt1 = 0 , cnt2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i]==el1) cnt1++;
+            if(nums[i]==el2) cnt2++;
+        }
+        int mini = (n/3+1);
+        if(cnt1>=mini){
+            res.push_back(el1);
+        }
+        if(cnt2 >= mini && el1!=el2){
+            res.push_back(el2);
+        }
+        return res;
+
     }
 };
